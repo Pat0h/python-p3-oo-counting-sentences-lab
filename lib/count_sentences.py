db9075907de230035084156a8e415759ab21918c
Pat0h <patrickngatia14@gmail.com>
2023-12-06 16:54:57 +0300

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
+# count_sentences.py
+
+# count_sentences.py
+
+# count_sentences.py
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        # Use regular expression to find sentences
+        sentences = re.split(r'[.!?]+', self._value)
+        # Remove empty strings from the list
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
